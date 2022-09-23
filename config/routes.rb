87056e99353aa9a373ceba1f1ff5a92355fb6694
Yuluki <web.yulukitaiga@gmail.com>
2022-09-23 21:39:16 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'trainings#index'
-  resources :trainings
+  resources :trainings do
+    resources :questions do
+      member do
+        post :answer
+      end
+    end
+  end
 end
