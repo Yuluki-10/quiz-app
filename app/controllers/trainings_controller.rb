54abbc843d53e3d4f635ab4702a7f761d64027f5
Yuluki <web.yulukitaiga@gmail.com>
2022-09-24 21:52:37 +0900

    トレーニングの作成から個別ページに表示まで

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,10 +1,32 @@
 class TrainingsController < ApplicationController
   def index
+    @trainings = Training.all
   end
 
+  # GET) トレーニング作成ページ
   def new
+    @training = Training.new
+  end
+
+  # POST) トレーニング作成
+  def create
+    @training = Training.create(training_params)
+    if @training.errors.any?
+      flash.now[:alert] = @training.errors.full_messages.join("\n")
+      render :new
+    else
+      flash[:notice] = "トレーニングが作成されました"
+      redirect_to trainings_path
+    end
+
   end
 
   def show
+    @training = Training.find(params[:id])
   end
+
+  private
+    def training_params
+      params.require(:training).permit(:title, :url)
+    end
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,2 +1,3 @@
 class Training < ApplicationRecord
+  validates :title, presence: true
 end
