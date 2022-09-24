f91aff634c4302300c046ee6e70036374fbe6a6e
Yuluki <web.yulukitaiga@gmail.com>
2022-09-24 11:13:03 +0900

    ページ遷移のテスト

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,7 @@
 class QuestionsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
 end
--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,10 @@
 class TrainingsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
 end
