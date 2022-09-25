516608c0506f0bb27b4ebf302899be5c430cf0b4
Yuluki <web.yulukitaiga@gmail.com>
2022-09-25 12:25:33 +0900

    問題・選択肢作成のための準備

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,8 @@
 class QuestionsController < ApplicationController
   def new
+    @training = Training.find(params[:training_id])
+    @question = Question.new
+    @choice = Choice.new
   end
 
   def show
