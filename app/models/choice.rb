41ca9c17804b006d4284b2e3df794e88c2801f4f
Yuluki <web.yulukitaiga@gmail.com>
2022-10-09 09:26:32 +0900

    【コメント】scopeメソッド

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -4,6 +4,7 @@ class Choice < ApplicationRecord
   validates :content, presence: true
   
   # 回答済みの相談を取得
+  # scopeメソッド。choice.correctedで正解解答を呼び出せる
   scope :corrected, ->{
     where(is_answer: true)
   }
