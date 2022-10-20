46ff92fc10760a743bb807ec9978854dc9b21333
Yuluki <web.yulukitaiga@gmail.com>
2022-10-20 15:39:52 +0900

    mypageの開始

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'chapters/new'
-  get 'chapters/edit'
-  get 'chapters/show'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'trainings#index'
   resources :trainings do
+    collection do
+      get :mypage
+    end
     resources :chapters do
       resources :questions do
         collection do
