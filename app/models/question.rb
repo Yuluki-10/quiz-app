be46ab66a805d5214c9f4c072b2ac24f8ed5b0b1
Yuluki <web.yulukitaiga@gmail.com>
2022-10-26 16:41:06 +0900

    クラスメソッドに切り替え？

--- app/models/question.rb
+++ app/models/question.rb
@@ -20,6 +20,11 @@ class Question < ApplicationRecord
     )
   }
 
+  # クラスメソッドに変更してみる
+  # def self.unanswered_method(userid) {
+
+  # }
+
   # currentユーザーが未回答の問題
   scope :unanswered, ->(userid) {
     # currentユーザーが答えていれば除外する
@@ -27,6 +32,7 @@ class Question < ApplicationRecord
     # where.missing(:user_answers)
     # orメソッドを使用するために、left_outer_joinsによる"左外部"結合が必要。missingは使用しない
     left_outer_joins(:user_answers).where(user_answers: {id: nil}).or(left_outer_joins(:user_answers).where.not(user_answers: {user_id: userid}))
+    # left_outer_joins(:user_answers).where(user_answers: {id: nil}).or(left_outer_joins(:user_answers).where.not(id: where(user_answer: {user_id: userid}).ids))
   }
 
 end
